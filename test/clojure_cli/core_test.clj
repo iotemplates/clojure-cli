90f76683b32f08369fd84fe138f62d5781b8b8b3
Ioannis Canellos <iocanel@gmail.com>
2021-09-19 12:52:00 +0300

    test: test validate-args function

--- test/clojure_cli/core_test.clj
+++ test/clojure_cli/core_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [clojure-template.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest validate-args-test
+  (testing "Testing failed"
+    ;; Expected to return exit message with failure status
+    (is (let [result (validate-args nil)]
+          (and
+           (contains? result :exit-message)
+           (not (:ok? result)))))
+    ;; Expected to display usage but not fail
+    (is (let [result (validate-args ["--help"])]
+          (and
+           (contains? result :exit-message)
+           (:ok? result))))))
+ 
+    
